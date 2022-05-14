f767692efc0e5c2823d611b115400ba6044b585e
AleLeonowicz <aleks.leonowicz@gmail.com>
2022-05-14 16:47:56 +0200

    Coding exercises 1

--- exercises.js
+++ exercises.js
@@ -166,3 +166,185 @@ const getSet = (arr) => {
 };
 
 console.log(getSet(array));
+
+console.log("--------------------------");
+
+/////////////////////////////////////////
+
+const exampleNr = 254680;
+
+const insertSignBetweenEvenNumbers = function (number) {
+  const nrToString = number.toString();
+  let newArr = [];
+
+  for (i = 1; i <= nrToString.length; i++) {
+    if (
+      +nrToString.slice(i - 1, i) % 2 === 0 &&
+      +nrToString.slice(i, i + 1) % 2 === 0 &&
+      i + 1 <= nrToString.length
+    ) {
+      newArr.push(nrToString.slice(i - 1, i));
+      newArr.push("-");
+    } else {
+      newArr.push(nrToString.slice(i - 1, i));
+    }
+  }
+  return newArr.join("");
+};
+
+console.log(insertSignBetweenEvenNumbers(exampleNr));
+
+const insertSignBetweenEvenNumbers2 = function (number) {
+  const nrToString = number.toString();
+  let newArr = [];
+
+  for (i = 0; i <= nrToString.length; i++) {
+    if (i !== 0 && nrToString[i] % 2 === 0 && nrToString[i - 1] % 2 === 0) {
+      console.log("current", nrToString[i]);
+      console.log("previous", nrToString[i - 1]);
+    }
+  }
+  return newArr.join("");
+};
+
+console.log(insertSignBetweenEvenNumbers2(exampleNr));
+
+console.log("--------------------------");
+
+/////////////////////////////////////////
+
+const testArr1 = [3, 8, 7, 6, 5, -4, -3, 2, 1];
+
+// const sortArr = function (arr) {
+//   let sortedArr = [];
+//   sortedArr.push(arr[0]);
+
+//   for (i = 1; i <= arr.length - 1; i++) {
+//     if (arr[i] < sortedArr[0]) {
+//       sortedArr.unshift(arr[i]);
+//     }
+//     if (arr[i] > sortedArr[0]) {
+//       sortedArr.push(arr[i]);
+//     }
+//   }
+//   return sortedArr;
+// };
+
+const sortArr = function (arr) {
+  newArr = [...arr];
+  newArr.sort(function (a, b) {
+    return a - b;
+  });
+  return newArr;
+};
+
+console.log(sortArr(testArr1));
+console.log(testArr1);
+
+console.log("--------------------------");
+
+/////////////////////////////////////////
+
+const testArr2 = [3, "a", "a", "a", 2, 3, "a", 3, "a", 2, 4, 9, 3];
+
+const findMostFrequentElement = function (arr) {
+  const helper = {};
+
+  arr.forEach((el) => {
+    helper[el] ? (helper[el] = helper[el] + 1) : (helper[el] = 1);
+  });
+
+  const helpersValues = Object.values(helper);
+  const max = Math.max(...helpersValues);
+  const index = helpersValues.indexOf(max);
+
+  const helpersKeys = Object.keys(helper);
+  const answer = helpersKeys[index];
+  console.log("answer", answer);
+  return answer;
+};
+
+findMostFrequentElement(testArr2);
+
+console.log("--------------------------");
+
+/////////////////////////////////////////
+
+const testString1 = "The Quick Brown Fox";
+
+const swapCaseInString = function (string) {
+  let newString = "";
+  for (i = 0; i <= string.length - 1; i++) {
+    string[i] === string[i].toLowerCase()
+      ? (newString = newString + string[i].toUpperCase())
+      : (newString = newString + string[i].toLowerCase());
+  }
+  console.log("newString", newString);
+  return newString;
+};
+
+swapCaseInString(testString1);
+
+console.log("--------------------------");
+
+/////////////////////////////////////////
+
+const testArr3 = [3, 8, 7, 6, 5, -4, -3, 2, 1];
+
+const giveSumAndProduct = function (arr) {
+  const sumAndProduct = {};
+
+  const sum = arr.reduce((cur, sum) => cur + sum, 0);
+  sumAndProduct.sum = sum;
+
+  console.log(sumAndProduct);
+
+  const product = arr.reduce((cur, product) => cur * product);
+  sumAndProduct.product = product;
+
+  console.log(sumAndProduct);
+};
+
+giveSumAndProduct(testArr3);
+
+console.log("--------------------------");
+
+/////////////////////////////////////////
+
+const testArr4 = [
+  3,
+  "alA",
+  "aLA",
+  "Ala",
+  2,
+  "ala",
+  3,
+  "AlA",
+  3,
+  "ALA",
+  2,
+  4,
+  9,
+  3,
+  "ala",
+];
+
+const removeDuplicates = function (arr) {
+  let lowerCaseArr = [];
+  let newArr = [];
+
+  for (i = 0; i <= arr.length - 1; i++) {
+    if (typeof arr[i] === "string") {
+      lowerCaseArr.push(arr[i].toLowerCase());
+    } else {
+      lowerCaseArr.push(arr[i]);
+    }
+    if (!newArr.includes(lowerCaseArr[i])) {
+      newArr.push(lowerCaseArr[i]);
+    }
+  }
+  console.log("newArr", newArr);
+  return newArr;
+};
+
+removeDuplicates(testArr4);
